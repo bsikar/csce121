c81528eff26bb7f845e86d16c0b47d428034abc3
Brighton Sikarskie <bsikar@tuta.io>
2023-02-26 14:20:56 -0600

    zybook 4

---
+++ zybooks/4. Input and Output Streams (which incorporate Object Oriented Concepts)/4.4 Input string stream/4.4.2_Reading_from_a_string.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main() {
+  istringstream inSS;
+  string userInput;
+  string userMonth;
+  int userDate;
+  int userYear;
+
+  getline(cin, userInput);
+  inSS.str(userInput);
+
+  inSS >> userMonth >> userDate >> userYear;
+
+  cout << "Month: " << userMonth << endl
+       << "Date: " << userDate << endl
+       << "Year: " << userYear << endl;
+
+  return 0;
+}
