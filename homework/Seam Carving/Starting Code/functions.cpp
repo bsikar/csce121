31f8692508ac573c304e00ad3776f92a361d77e7
Brighton Sikarskie <bsikar@tuta.io>
2023-03-10 00:02:27 -0600

    format

--- homework/Seam Carving/Starting Code/functions.cpp
+++ homework/Seam Carving/Starting Code/functions.cpp
@@ -20,12 +20,12 @@ void initializeImage(Pixel image[][MAX_HEIGHT]) {
 }
 
 void printImage(const Pixel image[][MAX_HEIGHT], unsigned int width,
-    unsigned int height) {
+                unsigned int height) {
   for (unsigned int row_index = 0; row_index < height; row_index++) {
     for (unsigned int col_index = 0; col_index < width; col_index++) {
       cout << "{" << image[col_index][row_index].r << ", "
-        << image[col_index][row_index].g << ", "
-        << image[col_index][row_index].b << "}";
+           << image[col_index][row_index].g << ", "
+           << image[col_index][row_index].b << "}";
       if (col_index < width - 1) {
         cout << ", ";
       }
@@ -36,52 +36,53 @@ void printImage(const Pixel image[][MAX_HEIGHT], unsigned int width,
 
 void loadImage(string filename, Pixel image[][MAX_HEIGHT], unsigned int &width,
                unsigned int &height) {
-    std::ifstream input_file(filename);
+  std::ifstream input_file(filename);
 
-    if (!input_file) {
-        throw std::runtime_error("Failed to open " + filename);
-    }
+  if (!input_file) {
+    throw std::runtime_error("Failed to open " + filename);
+  }
 
-    // Read in the PPM header information
-    string line;
-    int maxColorValue;
-    input_file >> line >> width >> height >> maxColorValue;
+  // Read in the PPM header information
+  string line;
+  int maxColorValue;
+  input_file >> line >> width >> height >> maxColorValue;
 
-    // Validate the header information
-    if (line != "P3" && line != "p3") {
-        throw std::runtime_error("Invalid type " + line);
-    }
-    if (width <= 0 || height <= 0 || width > MAX_WIDTH || height > MAX_HEIGHT) {
-        throw std::runtime_error("Invalid dimensions");
-    }
-    if (maxColorValue != 255) {
+  // Validate the header information
+  if (line != "P3" && line != "p3") {
+    throw std::runtime_error("Invalid type " + line);
+  }
+  if (width <= 0 || height <= 0 || width > MAX_WIDTH || height > MAX_HEIGHT) {
+    throw std::runtime_error("Invalid dimensions");
+  }
+  if (maxColorValue != 255) {
+    throw std::runtime_error("Invalid color value");
+  }
+
+  // Read in the pixel values
+  int red, green, blue;
+  for (unsigned int j = 0; j < height; j++) {
+    for (unsigned int i = 0; i < width; i++) {
+      if (!(input_file >> red >> green >> blue)) {
         throw std::runtime_error("Invalid color value");
+      }
+      if (red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 ||
+          blue > 255) {
+        throw std::runtime_error("Invalid color value");
+      }
+      image[i][j].r = red;
+      image[i][j].g = green;
+      image[i][j].b = blue;
     }
+  }
 
-    // Read in the pixel values
-    int red, green, blue;
-    for (unsigned int j = 0; j < height; j++) {
-        for (unsigned int i = 0; i < width; i++) {
-            if (!(input_file >> red >> green >> blue)) {
-                throw std::runtime_error("Invalid color value");
-            }
-            if (red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255) {
-                throw std::runtime_error("Invalid color value");
-            }
-            image[i][j].r = red;
-            image[i][j].g = green;
-            image[i][j].b = blue;
-        }
-    }
+  // Check if there are too many pixel values
+  if (input_file >> red) {
+    throw std::runtime_error("Too many values");
+  }
 
-    // Check if there are too many pixel values
-    if (input_file >> red) {
-        throw std::runtime_error("Too many values");
-    }
+  printImage(image, width, height);
 
-    printImage(image, width, height);
-
-    input_file.close();
+  input_file.close();
 }
 
 void outputImage(string filename, Pixel image[][MAX_HEIGHT], unsigned int width,
